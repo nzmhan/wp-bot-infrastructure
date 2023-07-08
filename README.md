af92f2bbd20f45b0f68a508d1026c171f2006e4c
Nazım <nazimhan8@gmail.com>
2023-07-08 18:51:17 +0300

    wp-bot-infrastructure

---
+++ README.md
@@ -0,0 +1,3 @@
+# wp-bot-infrastructure
+A whatsapp bot infrastructure which supports slash commands, and it has event and command handler so that it is really easy to add new commands/events.
+
---
+++ src/functions/basicFn.ts
@@ -0,0 +1,42 @@
+import { Message } from "whatsapp-web.js"
+import * as fs from "fs"
+import * as path from "path"
+
+function getArgs(message: Message): string[]{
+  const [,...args] = message.body.split(/\s| |\n/)
+  return args
+}
+
+function getAllFileAbsDirs(folderPath) {
+  let folder = fs.readdirSync(folderPath)
+  let fileDirs: string[] = []
+  for (let file of folder){
+    let filePath = path.join(folderPath, file)
+    if (fs.lstatSync(filePath).isDirectory()){
+      fileDirs.push(...getAllFileAbsDirs(filePath))
+    }
+    else {
+      fileDirs.push(filePath)
+    }
+  }
+  return fileDirs
+}
+
+function getAllFileRelDirs(folderPath){
+  const fileDirs = getAllFileAbsDirs(folderPath)
+  for (let i in fileDirs)
+    fileDirs[i] = path.relative(__dirname, fileDirs[i])
+  return fileDirs
+}
+
+
+// const pth = path.join(__dirname, "../events")
+//
+// console.log(getAllFileAbsDirs(pth))
+// console.log(getAllFileRelDirs(pth))
+//
+
+
+
+
+export {getArgs, getAllFileRelDirs, getAllFileAbsDirs}
